ec9ce0a0d4c165102709de031589ef073c2edbf0
ouchafra <ismail.soso9@gmail.com>
2023-03-28 20:02:09 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -10,6 +10,6 @@ void _puts(char *str)
 	while (*str != '\0')
 	{
 		_putchar(*str++);
-	{
+	}
 		_putchar('\n');
 }
--- 0x05-pointers_arrays_strings/_putchar.c
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -1,4 +1,4 @@
-I#include "main.h"
+#include "main.h"
 #include <unistd.h>
 /**
  * _putchar - writes the character c to stdout
