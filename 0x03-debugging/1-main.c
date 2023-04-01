98e092aa5733bce713ea188d54404b5efb60225b
ouchafra <ismail.soso9@gmail.com>
2023-04-01 02:52:57 +0000

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
-* main - causes an infinite loop
-* Return: 0
-*/
+ * main - causes an infinite loop
+ * Return: 0
+ */
 
 int main(void)
 {
@@ -14,7 +14,7 @@ int main(void)
 	i = 0;
 
 	while (i < 10)
-        {
+	{
 		putchar(i);
 	}
 
